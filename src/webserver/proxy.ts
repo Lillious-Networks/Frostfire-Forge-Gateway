54d667f97d23b6216f2dcb01528fa77375b6728d
Logan Brown <github@lilliousnetworks.com>
2026-08-20 14:33:56 -0400

    Update proxy

--- src/webserver/proxy.ts
+++ src/webserver/proxy.ts
@@ -46,7 +46,6 @@ Bun.serve({
   port: publicPort,
   development: false,
   reusePort: false,
-  http3: true,
   async fetch(req: Request, server: any) {
     const ip = server.requestIP(req)?.address;
     const url = tryParseURL(req.url);
