c391837d1e0e8d1b626682f257dbfa69c64d111f
Logan Brown <github@lilliousnetworks.com>
2026-09-11 17:44:05 -0400

    Update css

--- src/webserver/public/docs/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Router, routes } from './router.js';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router routes={routes} />
-  </React.StrictMode>
-);
